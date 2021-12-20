f15fb3d9dacd8f733b96d15bf0f3054470249660
mohitrathor98 <mohitrathor8@gmail.com>
2021-12-20 16:23:32 +0530

    replacing cards

--- main.py
+++ main.py
@@ -4,19 +4,28 @@ import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 
-#-------------------Generating New Words---------------------#
 data = pandas.read_csv("data/french_words.csv")
 data_list= data.to_dict(orient='records') # [list of dictionaries{keys: 'French', 'English}]
 
+#-------------------Change canvas items----------------------#
+
+def replace_canvas_items(transition_image, transition_lang, transition_word, text_color = "black"):
+    flash_card.itemconfig(canvas_image, image=transition_image)
+    flash_card.itemconfig(language,text=transition_lang, fill=text_color)
+    flash_card.itemconfig(word, text=transition_word, fill=text_color)
+
+def show_answer():
+    replace_canvas_items(flash_back_img, "English", data_list[0]["English"], text_color="white")
+
 def right():
-    letter_dict = data_list[0]
-    flash_card.itemconfig(word, text=letter_dict["French"])
-    data_list.remove(letter_dict)
+    data_list.remove(data_list[0])
+    replace_canvas_items(flash_front_img, "French", data_list[0]["French"])
+    timer = window.after(3000, func=show_answer)
 
 def wrong():
-    letter_dict = data_list[0]
-    flash_card.itemconfig(word, text=letter_dict['French'])
     random.shuffle(data_list)
+    replace_canvas_items(flash_front_img, "French", data_list[0]["French"])
+    timer = window.after(3000, func=show_answer)
     
 
 #--------------------------UI Setup--------------------------#
@@ -27,7 +36,8 @@ window.config(padx=50, pady= 50, bg=BACKGROUND_COLOR)
 # flash card
 flash_card = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 flash_front_img = PhotoImage(file="images/card_front.png")
-flash_card.create_image(410, 270,image=flash_front_img)
+flash_back_img = PhotoImage(file="images/card_back.png")
+canvas_image = flash_card.create_image(410, 270,image=flash_front_img)
 language = flash_card.create_text(400, 150, text="French", font=("Ariel", 40, "italic"))
 word = flash_card.create_text(400, 263, text="trouve", font=("Ariel", 60, "bold"))
 flash_card.grid(column=0, row=0, columnspan=2)
@@ -43,19 +53,7 @@ tick_button = Button(image=tick_img, highlightthickness=0, bg=BACKGROUND_COLOR, 
 tick_button.grid(column=1, row=1)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+timer = window.after(3000, func=show_answer)
 
 
 
